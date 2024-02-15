8d0a2ecf383a425ed67f8f594953e0380cc4dac0
Ermias <101344704+Eermias@users.noreply.github.com>
2024-02-15 16:25:07 +0300

    Add solution for Maximum Element After Decreasing and Rearranging

--- leetcode/maximum-element-after-decreasing-and-rearranging.py
+++ leetcode/maximum-element-after-decreasing-and-rearranging.py
@@ -1,11 +1,27 @@
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
-        arr.sort()
-        for i in range(len(arr)):
-            if i == 0:
-                arr[i] = 1
+        maxx = len(arr)
+        surplus = 0
+        mapp = {}
+        for num in arr:
+            mapp[num] = 1 + mapp.get(num, 0)
+            if num > maxx:
+                surplus += 1
+        #print(mapp, maxx, surplus)
+        for i in range(len(arr), 0, -1):
+            if i not in mapp: #O(1)
+                if surplus > 0:
+                    surplus -= 1
+                else:
+                    #if maxx - i >= 2:
+                        #surplus += 1
+                    maxx -= 1
+                    print(i, maxx)
             else:
-                if arr[i] - arr[i - 1] >= 2:
-                    arr[i] -= (arr[i] - arr[i - 1] - 1)
-        return arr[-1]
+                surplus += mapp[i] - 1
+        return maxx
+
+                
+        
+        
         
