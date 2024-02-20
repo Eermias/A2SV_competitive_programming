a0ff25c4337ec5546e1747110bf22fb93e9dda04
Ermias <101344704+Eermias@users.noreply.github.com>
2024-02-20 07:44:42 -0800

    Add solution for Reverse String

--- leetcode/reverse-string.py
+++ leetcode/reverse-string.py
@@ -3,13 +3,19 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
 
-        def rec(l,r):
+        """def rec(l,r):
             if l >= r:
                 return
             s[l], s[r] = s[r], s[l]
             rec(l + 1, r - 1)
-        rec(0, len(s) -1)
+        rec(0, len(s) -1)"""
 
         
 
