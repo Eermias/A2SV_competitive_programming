4cab928417035ed9d9fb4eee0cdbed4dc10413b6
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-02-25 11:40:14 -0800

    Add solution for Minimum Number of Pushes to Type Word I

---
+++ leetcode/minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        n = len(word)
+        if n <= 8:
+            return n
+        elif n <= 16:
+            return (n - 8) * 2 + 8
+        elif n <= 24:
+            return (n - 16) * 3 + 24
+        else:
+            return (n - 24) * 4 + 48
+        
