40f4df3b0b1d2b8d3b9caba94a5d9dd2aad5ea9f
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-03-04 13:00:05 +0300

    Add solution for Plus One

--- leetcode/plus-one.py
+++ leetcode/plus-one.py
@@ -1,12 +1,25 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        res = ""
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1] + digits
+        
+
+
+
+
+        #WEAK, this is cheating 
+        """res = ""
         for n in digits:
             res += str(n)
         res= str(int(res) + 1)
         ans = [int(x) for x in list(res)]
 
-        return ans
+        return ans"""
 
           
         
