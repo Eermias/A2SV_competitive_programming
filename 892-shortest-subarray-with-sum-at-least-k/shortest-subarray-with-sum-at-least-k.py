e0afc6a303bd89cf88b8197dbae7f1eb71204b05
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-06-23 09:58:26 +0300

    Time: 868 ms (80.35%) | Memory: 30.6 MB (98.10%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -1,18 +1,18 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        starts = deque([[-1, 0]]) #[index, prefix]
-        prefix = 0
+        window = deque()
+        window.append([-1, 0])
+        curr_sum = 0
         shortest = float("inf")
         for i, n in enumerate(nums):
-            prefix += n
-            while starts and prefix - starts[0][1] >= k:
-                shortest = min(shortest, i - starts[0][0])
-                starts.popleft()
-            while starts and prefix < starts[-1][1]:
-                starts.pop()
-            starts.append([i, prefix])
+            curr_sum += n
+            while window and curr_sum <= window[-1][1]:
+                window.pop()
+            #print(window)
+            while window and curr_sum - window[0][1] >= k:
+                idx, prefix = window.popleft()
+                shortest = min(shortest, i - idx)
+            window.append([i, curr_sum])
 
-        if shortest == float("inf"):
-            shortest = -1
-        return shortest
+        return shortest if shortest != float("inf") else -1
 
