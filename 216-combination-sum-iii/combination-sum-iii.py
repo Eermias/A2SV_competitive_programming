aee7fd56eb3aab6b5e1544fcf4554d08cd26f4a2
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-06-24 09:54:59 +0300

    Time: 43 ms (14.84%) | Memory: 16.4 MB (57.02%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,5 +1,21 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        res = []
+        curr = []
+        def dfs(num, total, count):
+            if count == k or total > n:
+                if total == n:
+                    res.append(curr[:])
+                return
+            for i in range(num, 10):
+                curr.append(i)
+                dfs(i + 1, total + i, count + 1)
+                curr.pop()
+        dfs(1, 0, 0)
+        return res
+
+"""class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         result = []
         curr = []
         def backtrack(i, curr_sum):
@@ -16,5 +32,5 @@ class Solution:
             backtrack(i + 1, curr_sum)
         
         backtrack(1, 0)
-        return result
+        return result"""
             
