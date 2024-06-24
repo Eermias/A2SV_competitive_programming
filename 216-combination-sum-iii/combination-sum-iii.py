59d7a446ed8e0c46123b242fa5d67ad69a77f9c1
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-06-24 09:58:18 +0300

    Time: 31 ms (85.01%) | Memory: 16.4 MB (57.02%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,16 +1,20 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        res = []
+        result = []
         curr = []
-        def dfs(num, total, count):
-            if count == k or total > n:
-                if total == n:
-                    res.append(curr[:])
+        def backtrack(i, curr_sum):
+            if len(curr) == k:
+                if curr_sum == n:
+                    result.append(curr[:])
+                return 
+            if i > 9:
                 return
-            for i in range(num, 10):
-                curr.append(i)
-                dfs(i + 1, total + i, count + 1)
-                curr.pop()
-        dfs(1, 0, 0)
-        return res
-
+            
+            curr.append(i)
+            backtrack(i + 1, curr_sum + i)
+            curr.pop()
+            backtrack(i + 1, curr_sum)
+        
+        backtrack(1, 0)
+        return result
+            
