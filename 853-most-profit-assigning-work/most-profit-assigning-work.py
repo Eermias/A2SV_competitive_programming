b6899ba09fd4ce9803734d01556f327e67b494a4
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-06-26 09:27:57 +0300

    Time: 269 ms (90.10%) | Memory: 19.4 MB (89.78%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.py
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -3,19 +3,14 @@ class Solution:
         mix = [[difficulty[i], profit[i]] for i in range(len(profit))]
         mix.sort()
         worker.sort()
+
         best_job = 0
-        
         max_profit = 0
-        ptr1, ptr2 = 0, 0
-        while ptr1 < len(worker) and ptr2 < len(mix):
-            if worker[ptr1] >= mix[ptr2][0]:
-                best_job = max(best_job, mix[ptr2][1])
-                ptr2 += 1
-            else:
-                ptr1 += 1
-                max_profit += best_job
-                
-        if ptr1 < len(worker):
-            max_profit += (len(worker) - ptr1) * (best_job)
-        
+        ptr = 0
+        for i in range(len(worker)):
+            while ptr < len(mix) and mix[ptr][0] <= worker[i]:
+                best_job = max(best_job, mix[ptr][1])
+                ptr += 1
+            max_profit += best_job
+            
         return max_profit
