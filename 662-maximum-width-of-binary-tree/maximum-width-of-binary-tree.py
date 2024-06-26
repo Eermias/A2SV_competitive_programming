33cb1d112f0ccd70b0b131057e82a280c76da913
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-06-26 19:02:14 +0300

    Time: 39 ms (79.00%) | Memory: 17.2 MB (87.89%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -6,24 +6,18 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        levels = defaultdict(list)
-
-        def dfs(node, pos, lvl):
-            if not node:
-                return
-            
-            levels[lvl].append(pos)
-            
-            dfs(node.left, 2 * pos, lvl + 1)
-            dfs(node.right, 2 * pos + 1, lvl + 1)
-            
-
-        
-        dfs(root, 0, 0)
-        #print(levels)
+        q = deque()
+        q.append([root, 0])
         max_width = 0
-        for key in levels:
-            level = levels[key]
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node, idx = q.popleft()
+                level.append(idx)
+                if node.left:
+                    q.append([node.left, idx * 2])
+                if node.right:
+                    q.append([node.right, idx * 2 + 1])
             max_width = max(max_width, level[-1] - level[0] + 1)
         
         return max_width
