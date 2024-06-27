ca8e5e80c7083e4d705190743e6cab226065bcf0
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-06-27 09:55:15 +0300

    Time: 42 ms (40.30%) | Memory: 17.4 MB (98.45%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
@@ -1,30 +1,16 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        ans = [0]
-        def dfs(node):
-            if not node:
-                return None
-            
-            result = [node.val, node.val]
-            left = dfs(node.left)
-            right = dfs(node.right)
+        maxVal = 0
 
-            if left:
-                ans[0] = max(ans[0], abs(node.val - left[0]), abs(node.val - left[1]))
-                result[0] = min(result[0], left[0])
-                result[1] = max(result[1], left[1])
-            if right:
-                ans[0] = max(ans[0], abs(node.val - right[0]), abs(node.val - right[1]))
-                result[0] = min(result[0], right[0])
-                result[1] = max(result[1], right[1])
-            
-            return result
-        
-        dfs(root)
-        return ans[0]
+        queue = deque([(root, root.val, root.val)])
+        while queue:
+            curr, high, low = queue.popleft()
+            currVal = curr.val
+            diff1, diff2 = currVal - low, high - currVal
+            maxVal = max(maxVal, diff1, diff2)
+
+            if curr.left:
+                queue.append((curr.left, max(currVal, high), min(currVal, low)))
+            if curr.right:
+                queue.append((curr.right, max(currVal, high), min(currVal, low)))
+        return maxVal
