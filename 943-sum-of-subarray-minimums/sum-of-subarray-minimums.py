178f123dda18ef9bfa53ef06fbf37f61cf08034b
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-06-28 17:32:04 +0300

    Time: 332 ms (60.33%) | Memory: 21.8 MB (9.36%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -5,12 +5,16 @@ class Solution:
         for i, n in enumerate(arr):    
             while stack and stack[-1][0] > n:
                 num, idx = stack.pop()
-                min_sum += (i - idx) * (idx - stack[-1][1]) * num
+                left = i - idx
+                right = idx - stack[-1][1]
+                min_sum += left * right * num
             
             stack.append([n, i])
             
         for i in range(1, len(stack)):
             num, idx = stack[i]
-            min_sum += (len(arr) - idx) * (idx - stack[i - 1][1]) * num
+            left = len(arr) - idx
+            right = idx - stack[i - 1][1]
+            min_sum += left * right * num
 
         return min_sum % (10**9 + 7)
