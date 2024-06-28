a0316c1d5e2e2b9dedc926b42f2b8d6c79b3b9e2
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-06-28 08:59:48 +0300

    Time: 37 ms (72.47%) | Memory: 16.8 MB (20.93%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,12 +1,16 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
+        memo = {}
         def pick(l, r):
+            if (l, r) in memo:
+                return memo[(l, r)]
             if l > r:
                 return 0
             left = nums[l] - pick(l + 1, r)
             right = nums[r] - pick(l, r - 1)
             
-            return max(left, right)
+            memo[(l, r)] = max(left, right)
+            return memo[(l, r)]
         
         if pick(0, len(nums) - 1) >= 0:
             return True
