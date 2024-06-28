0639ba3c56e4ea06777fcf7508b109ea440df900
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-06-28 17:54:04 +0300

    Time: 328 ms (68.50%) | Memory: 21.5 MB (68.90%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -1,6 +1,7 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        stack = [[float("-inf"), -1]]
+        arr = [float("-inf")] + arr + [float("-inf")]
+        stack = []
         min_sum = 0
         for i, n in enumerate(arr):    
             while stack and stack[-1][0] > n:
@@ -8,9 +9,5 @@ class Solution:
                 min_sum += (i - idx) * (idx - stack[-1][1]) * num
             
             stack.append([n, i])
-            
-        for i in range(1, len(stack)):
-            num, idx = stack[i]
-            min_sum += (len(arr) - idx) * (idx - stack[i - 1][1]) * num
 
         return min_sum % (10**9 + 7)
