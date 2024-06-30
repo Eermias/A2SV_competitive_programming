8f2693c8810c029325c2004493515e1bf6e53de2
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-06-30 12:22:06 +0300

    Time: 487 ms (82.16%) | Memory: 36.9 MB (65.01%) - LeetSync

--- 456-132-pattern/132-pattern.py
+++ 456-132-pattern/132-pattern.py
@@ -1,17 +1,11 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        stack = [] #[num, min_popped]
-
-        for n in nums:
-            minn = n
-            while stack and stack[-1][0] <= n:
-                num, min_pop = stack.pop()
-                minn = min(minn, min_pop)
-            
-            if stack and stack[-1][1] < n:
-                return True
-            stack.append([n, minn])
-        
+        stack = []
+        pre_max = -math.inf
+        for num in nums[::-1]:
+            if num < pre_max: return True
+            while stack and stack[-1]<  num:
+                # pre_max = max(pre_max, stack.pop())
+                pre_max =  stack.pop()
+            stack.append(num)  
         return False
-
-
