db34af75c8b8d8ce1ffca68030c4baf76c994860
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-02 16:55:33 +0300

    Time: 108 ms (27.06%) | Memory: 17.4 MB (47.51%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,40 +4,30 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         ROWS, COLS = len(matrix), len(matrix[0])
+        row1, col1 = False, False
+        for r in range(ROWS):
+            if matrix[r][0] == 0:
+                col1 = True
+        
+        for c in range(COLS):
+            if matrix[0][c] == 0:
+                row1 = True
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if matrix[r][c] == 0:
-                    matrix[r][c] = str(matrix[r][c])
-                    matrix[r][0] += "R"
-                    matrix[0][c] += "C"
-                else:
-                    matrix[r][c] = str(matrix[r][c])
-    
-        for r in range(1, ROWS):
-            for c in range(1, COLS):
-                if "R" in matrix[r][0] or "C" in matrix[0][c]:
-                    matrix[r][c] = 0
-
-        for r in range(1, ROWS):
-            if "R" in matrix[r][0]:
-                matrix[r][0] = 0
+                    matrix[r][0] = 0
+                    matrix[0][c] = 0
         
-        for c in range(1, COLS):
-            if "C" in matrix[0][c]:
-                matrix[0][c] = 0
-
-        if "R" in matrix[0][0]:
+        for r in range(1, ROWS):
             for c in range(1, COLS):
-                matrix[0][c] = 0
-
-        if "C" in matrix[0][0]:
-            for r in range(1, ROWS):
-                matrix[r][0] = 0
-
-        if "R" in matrix[0][0] or "C" in matrix[0][0]:
-            matrix[0][0] = 0
-
-        #print(matrix)
-        for r in range(ROWS):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+        
+        if row1:
             for c in range(COLS):
-                matrix[r][c] = int(matrix[r][c])
+                matrix[0][c] = 0
+            
+        if col1:
+            for r in range(ROWS):
+                matrix[r][0] = 0
