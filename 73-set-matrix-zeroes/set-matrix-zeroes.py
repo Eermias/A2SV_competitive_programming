0a7d85858579c84cadaf9def9571e114cfe41405
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-02 16:41:46 +0300

    Time: 112 ms (14.50%) | Memory: 18.9 MB (15.44%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,15 +4,40 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         ROWS, COLS = len(matrix), len(matrix[0])
-        rows, cols = set(), set()
         for r in range(ROWS):
             for c in range(COLS):
                 if matrix[r][c] == 0:
-                    rows.add(r)
-                    cols.add(c)
+                    matrix[r][c] = str(matrix[r][c])
+                    matrix[r][0] += "R"
+                    matrix[0][c] += "C"
+                else:
+                    matrix[r][c] = str(matrix[r][c])
+    
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if "R" in matrix[r][0] or "C" in matrix[0][c]:
+                    matrix[r][c] = 0
 
+        for r in range(1, ROWS):
+            if "R" in matrix[r][0]:
+                matrix[r][0] = 0
+        
+        for c in range(1, COLS):
+            if "C" in matrix[0][c]:
+                matrix[0][c] = 0
+
+        if "R" in matrix[0][0]:
+            for c in range(1, COLS):
+                matrix[0][c] = 0
+
+        if "C" in matrix[0][0]:
+            for r in range(1, ROWS):
+                matrix[r][0] = 0
+
+        if "R" in matrix[0][0] or "C" in matrix[0][0]:
+            matrix[0][0] = 0
+
+        #print(matrix)
         for r in range(ROWS):
             for c in range(COLS):
-                if r in rows or c in cols:
-                    matrix[r][c] = 0
-        
+                matrix[r][c] = int(matrix[r][c])
