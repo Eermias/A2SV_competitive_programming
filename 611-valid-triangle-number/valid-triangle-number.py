98aec6d3632cae9dbec69f7cd53c8d0ce4f12c79
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-02 15:30:32 +0300

    Time: 479 ms (72.40%) | Memory: 16.5 MB (81.34%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.py
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -1,21 +1,15 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        def binary_search(i, j):
-            l, r = j + 1, len(nums) - 1
-            k = j
-            while l <= r:
-                m = (l + r) // 2
-                if nums[m] < nums[i] + nums[j]:
-                    k = m
-                    l = m + 1
-                else:
-                    r = m - 1
-            return k
-            
         nums.sort()
         count = 0
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums) - 1):
-                k = binary_search(i, j)
-                count += k - j
+
+        for k in range(len(nums) - 1, 1, -1):
+            i, j = 0, k - 1
+            while i < j:
+                if nums[i] + nums[j] > nums[k]:
+                    count += j - i
+                    j -= 1
+                else:
+                    i += 1
+
         return count
