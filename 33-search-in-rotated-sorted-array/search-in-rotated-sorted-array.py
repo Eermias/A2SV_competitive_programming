d78a0604807b20ffe10bd258153c8f36755faed6
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-03 10:43:04 +0300

    Time: 44 ms (51.08%) | Memory: 16.9 MB (29.21%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -6,22 +6,22 @@ class Solution:
             if nums[m] == target:
                 return m
             elif nums[m] < target:
-                #if nums[m] is in the left rotated portion
+                #check if nums[m] is in the left rotated portion
                 if nums[r] <= nums[l] <= nums[m]:
                     l = m + 1
                 else:
-                    if target <= nums[r]:
-                        l = m + 1
-                    else:
+                    if target > nums[r]:
                         r = m - 1
+                    else:
+                        l = m + 1
             else:
-                #if nums[m] is in the left rotated portion
-                if nums[m] <= nums[r] <= nums[l]:
-                    r = m - 1
-                else:
-                    if target >= nums[l]:
-                        r = m - 1
-                    else:
+                #check if nums[m] is in the left rotated portion
+                if nums[r] <= nums[l] <= nums[m]:
+                    if target < nums[l]:
                         l = m + 1
-
+                    else:
+                        r = m - 1
+                else:
+                    r = m - 1
+        
         return -1
