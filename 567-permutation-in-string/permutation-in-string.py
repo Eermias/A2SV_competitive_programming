c49c12fac0ca330cf7043d82378e53a65cd5799f
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-04 16:55:44 +0300

    Time: 61 ms (60.40%) | Memory: 16.6 MB (86.06%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -10,28 +10,14 @@ class Solution:
             count1[ord(s1[i]) - ord('a')] += 1
             count2[ord(s2[i]) - ord('a')] += 1
 
-        same = 0
-        for i in range(26):
-            if count1[i] == count2[i]:
-                same += 1
-        
         l = 0
         for r in range(len(s1), len(s2)):
-            if same == 26:
+            if count1 == count2:
                 return True
-            
+
             count2[ord(s2[r]) - ord('a')] += 1
-            if count2[ord(s2[r]) - ord('a')] == count1[ord(s2[r]) - ord('a')]:
-                same += 1
-            elif count2[ord(s2[r]) - ord('a')] == count1[ord(s2[r]) - ord('a')] + 1:
-                same -= 1
-            
             count2[ord(s2[l]) - ord('a')] -= 1
-            if count2[ord(s2[l]) - ord('a')] == count1[ord(s2[l]) - ord('a')]:
-                same += 1
-            elif count2[ord(s2[l]) - ord('a')] + 1 == count1[ord(s2[l]) - ord('a')]:
-                same -= 1
             l += 1
         
-        return same == 26
+        return count1 == count2
             
