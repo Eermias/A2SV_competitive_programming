58c791b981c2844554b8367606662a8a488cfb76
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-04 16:54:34 +0300

    Time: 67 ms (50.83%) | Memory: 16.7 MB (52.31%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -9,15 +9,29 @@ class Solution:
         for i in range(len(s1)):
             count1[ord(s1[i]) - ord('a')] += 1
             count2[ord(s2[i]) - ord('a')] += 1
-        count2[ord(s2[len(s1) - 1]) - ord('a')] -= 1
 
+        same = 0
+        for i in range(26):
+            if count1[i] == count2[i]:
+                same += 1
+        
         l = 0
-        for r in range(len(s1) - 1, len(s2)):
-            count2[ord(s2[r]) - ord('a')] += 1
-            if count1 == count2:
+        for r in range(len(s1), len(s2)):
+            if same == 26:
                 return True
+            
+            count2[ord(s2[r]) - ord('a')] += 1
+            if count2[ord(s2[r]) - ord('a')] == count1[ord(s2[r]) - ord('a')]:
+                same += 1
+            elif count2[ord(s2[r]) - ord('a')] == count1[ord(s2[r]) - ord('a')] + 1:
+                same -= 1
+            
             count2[ord(s2[l]) - ord('a')] -= 1
+            if count2[ord(s2[l]) - ord('a')] == count1[ord(s2[l]) - ord('a')]:
+                same += 1
+            elif count2[ord(s2[l]) - ord('a')] + 1 == count1[ord(s2[l]) - ord('a')]:
+                same -= 1
             l += 1
         
-        return False
+        return same == 26
             
