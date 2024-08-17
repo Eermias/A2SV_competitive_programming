4f2d21c49725b5786c664cee69c3ab6305cceba3
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-17 21:05:47 +0300

    Time: 267 ms (21.84%) | Memory: 18 MB (93.98%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -4,11 +4,9 @@ class Solution:
         maxx = 0
         r = 1
         for l in range(len(nums) - 1):
-            while r < len(nums) and nums[r] == nums[l]:
-                r += 1
-
-            while r < len(nums) and nums[r] - nums[l] == 1:
-                maxx = max(maxx, r - l + 1)
+            while r < len(nums) and nums[r] - nums[l] < 2:
+                if nums[r] - nums[l] == 1:
+                    maxx = max(maxx, r - l + 1)
                 r += 1
         
         return maxx
