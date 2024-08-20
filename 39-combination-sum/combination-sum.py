b6c121f8b9c29e20fdaade09b58318c4b6d939d9
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-20 11:03:47 +0300

    Time: 59 ms (28.66%) | Memory: 16.7 MB (40.30%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,22 +2,17 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
         curr = []
-        def backtrack(i, curr_sum):
-            if curr_sum == target:
-                result.append(curr[:])
-                return
-            if curr_sum > target or i == len(candidates):
+        def backtrack(i, summ):
+            if i == len(candidates) or summ >= target:
+                if summ == target:
+                    result.append(curr[:])
                 return
             
             curr.append(candidates[i])
-            backtrack(i, curr_sum + candidates[i])
+            backtrack(i, summ + candidates[i])
+
             curr.pop()
-            backtrack(i + 1, curr_sum)
+            backtrack(i + 1, summ)
         
         backtrack(0, 0)
         return result
-            
-
-            
-                
-        
