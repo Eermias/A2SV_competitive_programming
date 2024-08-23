5c2213b4b65f62557c8e33cd6957ba71660f6880
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-23 08:35:46 +0300

    Time: 30 ms (85.48%) | Memory: 16.6 MB (10.37%) - LeetSync

--- 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
+++ 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
@@ -1,4 +1,11 @@
 class Solution:
+    def compute2Power(self, n):
+        if n==1:
+            return 2
+        tmp=(self.compute2Power(n//2)%(1000000007))
+        if n%2==0:   
+            return (tmp**2)%(1000000007)
+        return (2*((tmp**2)%(1000000007)))%(1000000007)
+
     def monkeyMove(self, n: int) -> int:
-        res = pow(2, n, 10**9 + 7) - 2
-        return res % (10**9 + 7)
+        return (self.compute2Power(n)-2)%(1000000007)
