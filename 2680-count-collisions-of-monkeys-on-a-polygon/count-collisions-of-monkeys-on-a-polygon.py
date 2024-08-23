414e57b255f4aa0908d8e722467ce7b2d0e3b81c
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-23 16:09:03 +0300

    Time: 41 ms (18.67%) | Memory: 16.6 MB (10.37%) - LeetSync

--- 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
+++ 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
@@ -1,11 +1,24 @@
 class Solution:
-    def compute2Power(self, n):
-        if n==1:
-            return 2
-        tmp=(self.compute2Power(n//2)%(1000000007))
-        if n%2==0:   
-            return (tmp**2)%(1000000007)
-        return (2*((tmp**2)%(1000000007)))%(1000000007)
-
     def monkeyMove(self, n: int) -> int:
-        return (self.compute2Power(n)-2)%(1000000007)
+        MOD = 10**9 + 7
+        def power(num):
+            if num <= 1:
+                return num + 1
+            
+            # important facts:
+            # (a * b) % MOD == [(a % MOD) * (b % MOD)] % MOD
+            # 2**n == 2**(n // 2) * 2**(n // 2) == (2**(n // 2)) ** 2 for even n
+            # 2**n == 2 * (2**(n // 2)) ** 2 for odd n
+            # (2**n) % MOD == (2**(n // 2) * 2**(n // 2)) % MOD == (a * b) % MOD 
+            # where a == b == 2**(n // 2)
+            # so (2**n) % MOD == (2**(n // 2) * 2**(n // 2)) % MOD == ((2**(n // 2) % MOD) ** 2) % MOD
+            # so 2**n % MOD = ((2**(n // 2) % MOD) ** 2) % MOD
+            # calculate temp = (2**(n // 2) % MOD) recursively and square it and take the modulo to MOD
+            # to get 2**n % MOD.....for odd n you need to multiply by two
+
+            temp = (power(num // 2) % (MOD))
+            if num % 2 == 0:   
+                return (temp**2) % (MOD)
+            return (2 * ((temp**2) % (MOD))) % (MOD)
+
+        return (power(n)-2) % (MOD)
