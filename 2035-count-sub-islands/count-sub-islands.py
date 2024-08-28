b76884b82a13632d2a4937586481450eeefad6f5
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-08-28 12:07:15 +0300

    Time: 2281 ms (29.70%) | Memory: 49.8 MB (36.85%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.py
+++ 2035-count-sub-islands/count-sub-islands.py
@@ -2,9 +2,9 @@ class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         def dfs(r, c):
             ans = True
-            if grid1[r][c] == 0:
+            if grid1[r][c] != 1:
                 ans = False
-
+            
             visited.add((r, c))
             for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 x = r + dr
@@ -14,13 +14,13 @@ class Solution:
                         ans = dfs(x, y) and ans
             
             return ans
-        
+
+
         visited = set()
         sub_islands = 0
         for r in range(len(grid1)):
             for c in range(len(grid1[0])):
                 if grid2[r][c] == 1 and (r, c) not in visited and dfs(r, c):
                     sub_islands += 1
-                    #print(r, c, visited)
         
         return sub_islands
