51b36f3dbefaef0b1f5a1a078d1894b6ece57353
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-09-04 09:34:33 +0300

    Time: 1045 ms (35.44%) | Memory: 48.8 MB (14.77%) - LeetSync

--- 2846-robot-collisions/robot-collisions.py
+++ 2846-robot-collisions/robot-collisions.py
@@ -2,32 +2,29 @@ class Solution:
     def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
         mix = [[positions[i], healths[i], i] for i in range(len(healths))]
         mix.sort()
-        survivors = []
+        right = []
+        left = []
         for p, h, i in mix:
-            if directions[i] == "R":
-                survivors.append([p, h, i])
+            if directions[i] == 'R':
+                right.append([i, h])
             else:
-                while survivors and directions[survivors[-1][2]] == "R" and survivors[-1][1] < h:
-                    survivors.pop()
+                while right and h and right[-1][1] < h:
+                    right.pop()
                     h -= 1
-                if not h:
+
+                if right and h == right[-1][1]:
+                    right.pop()
                     continue
-                elif not survivors:
-                    survivors.append([p, h, i])
+                elif right:
+                    right[-1][1] -= 1
                 else:
-                    dxn = directions[survivors[-1][2]]
-                    hlth = survivors[-1][1]
-                    if dxn == "L":
-                        survivors.append([p, h, i])
-                    elif hlth > h:
-                        survivors[-1][1] -= 1
-                        continue
-                    elif hlth == h:
-                        survivors.pop()
-                        continue
-        survivors.sort(key = lambda x:x[2])
-        result = [survivors[i][1] for i in range(len(survivors))]
-        return result
+                    left.append([i, h])
+        
+
+        survivors = right + left
+        survivors.sort()
+        survivors = [h for i, h in survivors]
+        return survivors
         
                     
 
