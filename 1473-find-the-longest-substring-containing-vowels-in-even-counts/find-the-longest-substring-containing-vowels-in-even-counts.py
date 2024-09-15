073d6776b2202580b258dbefcd5657d603f58b82
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-09-15 18:18:54 +0300

    Time: 1080 ms (5.01%) | Memory: 22.6 MB (10.58%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,19 +1,19 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
-        prev = {(0, 0, 0, 0, 0) : -1}
+        prev = {0 : -1}
         freq = defaultdict(int)
 
         longest = 0
         for i in range(len(s)):
             freq[s[i]] += 1
-            pattern = [0] * 5
+            mask = 0
             for j, c in enumerate("aeiou"):
                 if freq[c] % 2:
-                    pattern[j] = 1
+                    mask |= (1 << j)
             
-            if tuple(pattern) in prev:
-                longest = max(longest, i - prev[tuple(pattern)])
+            if mask in prev:
+                longest = max(longest, i - prev[mask])
             else:
-                prev[tuple(pattern)] = i
+                prev[mask] = i
         
         return longest
