38fbd56d32502997639e765f12749a25759cc0d1
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-09-19 11:16:02 +0100

    Time: 48 ms (64.15%) | Memory: 16.5 MB (41.23%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,15 +1,9 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        total = 0
         def backtrack(i, curr):
-            nonlocal total
             if i == len(nums):
-                total += curr
-                return
+                return curr
             
-            #pick or skip respectively
-            backtrack(i + 1, curr ^ nums[i])
-            backtrack(i + 1, curr)
+            return backtrack(i + 1, curr ^ nums[i]) + backtrack(i + 1, curr)
         
-        backtrack(0, 0)
-        return total
+        return backtrack(0, 0)
