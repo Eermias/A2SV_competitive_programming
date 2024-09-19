2b07c1aaa1b8655c5611cae28829874b8f73635b
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-09-19 11:32:15 +0100

    Time: 31 ms (90.40%) | Memory: 16.8 MB (23.90%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,16 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         power_set = []
-        sub_set = []
+        curr_set = []
         def backtrack(i):
             if i == len(nums):
-                power_set.append(sub_set[:])
+                power_set.append(curr_set[:])
                 return
             
-            sub_set.append(nums[i])
+            curr_set.append(nums[i])
             backtrack(i + 1)
 
-            sub_set.pop()
+            curr_set.pop()
             backtrack(i + 1)
         
         backtrack(0)
