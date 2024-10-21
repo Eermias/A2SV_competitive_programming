17011c2007e27af79405035b99ed564cb5e3b4dc
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-10-21 10:53:14 +0300

    Time: 63 ms (99.60%) | Memory: 23.8 MB (34.29%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.py
+++ 1002-maximum-width-ramp/maximum-width-ramp.py
@@ -1,12 +1,15 @@
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
-        sortedd = [(nums[i], i) for i in range(len(nums))]
-        sortedd.sort(reverse = True)
+        stack = []
+        for i in range(len(nums)):
+            if not stack or nums[i] < nums[stack[-1]]:
+                stack.append(i)
 
+        print(stack)
         max_width = 0
         for j in range(len(nums) - 1, -1, -1):
-            while sortedd and sortedd[-1][0] <= nums[j]:
-                val, idx = sortedd.pop()
+            while stack and nums[stack[-1]] <= nums[j]:
+                idx = stack.pop()
                 max_width = max(max_width, j - idx)
         
         return max_width
