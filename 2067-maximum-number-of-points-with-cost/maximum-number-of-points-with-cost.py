4448bf65b62b4e80d0456cb3643738ff9072b734
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-11-18 08:46:31 +0300

    Time: 384 ms (78.61%) | Memory: 40 MB (83.69%) - LeetSync

--- 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
@@ -6,17 +6,12 @@ class Solution:
                 for i in range(cols):
                     points[r][i] += points[r - 1][i]
 
-            left = points[r].copy()
             for i in range(cols - 2, -1, -1):
-                left[i] = max(left[i], left[i + 1] - 1)
+                points[r][i] = max(points[r][i], points[r][i + 1] - 1)
             
-            right = points[r].copy()
             for i in range(1, cols):
-                right[i] = max(right[i], right[i - 1] - 1)
+                points[r][i] = max(points[r][i], points[r][i - 1] - 1)
             
-            for i in range(cols):
-                points[r][i] = max(left[i], right[i])
-        
         return max(points[-1])
 
 
