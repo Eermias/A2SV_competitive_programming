147714a0347c63132bdf9f0eb03bbe6a5bf14c3b
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2024-11-22 20:44:16 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (50.93%) - LeetSync

--- 440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.py
+++ 440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.py
@@ -1,8 +1,8 @@
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
         def count(curr):
-            ngbr = curr + 1
             res = 0
+            ngbr = curr + 1
             while curr <= n:
                 res += min(ngbr, n + 1) - curr
                 curr *= 10
@@ -13,10 +13,10 @@ class Solution:
         curr = 1
         i = 1
         while i < k:
-            nodes = count(curr)
-            if i + nodes <= k:
+            steps = count(curr)
+            if i + steps <= k:
                 curr += 1
-                i += nodes
+                i += steps
             else:
                 curr *= 10
                 i += 1
