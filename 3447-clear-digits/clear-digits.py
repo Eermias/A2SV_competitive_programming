8778357074aacec10909995b209a30fed292beb4
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2025-02-10 17:28:10 +0300

    Time: 0 ms (100.00%) | Memory: 17.5 MB (87.42%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -1,18 +1,10 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        n = len(s)
-        removed = [False] * n
-        for i in range(n):
-            if s[i] in '0123456789':
-                removed[i] = True
-                for j in range(i - 1, -1, -1):
-                    if s[j] not in '0123456789' and not removed[j]:
-                        removed[j] = True
-                        break
-        
         result = []
-        for i in range(n):
-            if not removed[i]:
-                result.append(s[i])
+        for c in s:
+            if c in '0123456789':
+                result.pop()
+            else:
+                result.append(c)
         
         return ''.join(result)
