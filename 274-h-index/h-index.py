99266082d98104cd9f3d9ae306787635edd4416a
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2025-02-11 12:20:50 +0300

    Time: 0 ms (100.00%) | Memory: 18.2 MB (37.17%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,13 +1,16 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
-
         citations.sort()
+        for i in range(len(citations)):
+            if citations[i] < n:
+                n -= 1   
+        return n
 
-        for i in range(n):
-            if citations[i] >= n - i:
-                return n - i
-        return 0
+        # for i in range():
+        #     if citations[i] >= n - i:
+        #         return n - i
+        # return 0
 
 
 
