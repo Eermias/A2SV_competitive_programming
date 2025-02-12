2104c02f6823c619a209dab0ea2df8e7f99769a7
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2025-02-12 17:17:39 +0300

    Time: 20 ms (97.39%) | Memory: 17.8 MB (48.01%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -1,15 +1,25 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        a, b = 0, int(sqrt(2**31))
+        prime_factors = []
         
-        while a <= b:
-            product = a**2 + b**2
-            if product == c:
-                return True
+        if c == 0:
+            return True
 
-            if product < c:
-                a += 1
-            else:
-                b -= 1
+        while c % 2 == 0:
+            prime_factors.append(2)
+            c /= 2
         
-        return False
+        for i in range(3, int(sqrt(c)) + 1, 2):
+            while c % i == 0:
+                prime_factors.append(i)
+                c /= i
+        
+        if c >= 2:
+            prime_factors.append(int(c))
+        is_sum = True
+        for p in prime_factors:
+            if p % 4 == 3:
+                if prime_factors.count(p) % 2 == 1:
+                    is_sum = False
+        
+        return is_sum
