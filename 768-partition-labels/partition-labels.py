30ca9239c91e4824663d661ddf48a19a677e71e9
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2025-02-12 15:57:27 +0300

    Time: 3 ms (90.36%) | Memory: 17.8 MB (53.12%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,19 +1,16 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        dic={}
-
+        dic = {}
         for i in range(len(s)):
-            dic[s[i]]=i
+            dic[s[i]] = i
         
-        last_position=0
-        ans=[]
-        start=0
-
+        last_pos = 0
+        start = -1
+        parts = []
         for i in range(len(s)):
-            last_position=max(last_position,dic[s[i]])
-
-            if i==last_position:
-                ans.append(last_position-start+1)
-                start=i+1
+            last_pos = max(last_pos, dic[s[i]])
+            if last_pos == i:
+                parts.append(i - start)
+                start = i
         
-        return ans
+        return parts
