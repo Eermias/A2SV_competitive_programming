b6e62f4490abefaac21cce02be3db692f68a54a0
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2025-02-14 12:05:21 +0300

    Time: 71 ms (67.20%) | Memory: 18.1 MB (75.38%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -4,12 +4,6 @@ class Solution:
         if len(s) < len(t):
             return ""
 
-        def is_valid(win, temp):
-            for c in temp:
-                if temp[c] > win[c]:
-                    return False
-            return True
-
         t_count = defaultdict(int)
         for c in t:
             t_count[c] += 1
@@ -18,14 +12,21 @@ class Solution:
         left = 0
         ans = [-1, -1]
         shortest = float('inf')
+        valid_count = 0
         for  right in range(len(s)):
             window[s[right]] += 1
-            while is_valid(window, t_count):
+            if s[right] in t_count and window[s[right]] == t_count[s[right]]:
+                valid_count += 1
+
+            while valid_count == len(t_count):
                 if right - left + 1 < shortest:
                     shortest = right - left + 1
                     ans = [left, right]
 
                 window[s[left]] -= 1
+                if s[left] in t_count and window[s[left]] < t_count[s[left]]:
+                    valid_count -= 1
+
                 left += 1
         
         if ans[0] == -1:
