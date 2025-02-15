306817ddc3e5d95c0a45ed42b40cf352ba8834bd
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2025-02-15 23:38:54 +0300

    Time: 1198 ms (35.72%) | Memory: 17.7 MB (68.86%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        
+        def backtrack(i, curr, num, ii):
+            if i == len(num):
+                if curr == ii:
+                    return True
+                return False
+            
+            temp = False
+            for j in range(i + 1, min(i + 4, len(num)) + 1):
+                temp = temp or backtrack(j, curr + int(num[i:j]), num, ii)
+            return temp
+            
+
+        result = 0
+        for i in range(1, n + 1):
+            if backtrack(0, 0, str(i * i), i):
+                result += i * i
+        
+        return result
