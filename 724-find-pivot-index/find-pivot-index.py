0121a9df616665849cdf1aa5662516d5c7abc5f7
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2025-02-17 11:15:28 +0300

    Time: 8 ms (41.52%) | Memory: 18.7 MB (38.96%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -5,10 +5,7 @@ class Solution:
             nums[i] += nums[i - 1]
         
         for i in range(1, len(nums)):
-            left_sum = nums[i - 1]
-            right_sum = nums[-1] - nums[i]
-
-            if left_sum == right_sum:
+            if nums[i - 1] == nums[-1] - nums[i]:
                 return i - 1
             
             left_sum = nums[i]
