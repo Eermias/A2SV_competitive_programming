c5bd09816f50b0570514ca70af9e21efd5310645
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2025-06-01 13:30:57 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.46%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        memo = {0:0, 1:1, 2:1}
-        if n in memo:
-            return memo[n]
+        if n <= 2:
+            return min(n, 1)
         
         one, two, three = 0, 1, 1
         for i in range(3, n + 1):
