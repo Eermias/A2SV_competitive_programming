1bd138071b6d5207bd5e304eddf6499a90d84c28
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2025-06-06 18:27:29 +0300

    Time: 746 ms (62.19%) | Memory: 18.1 MB (68.57%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,25 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n = len(coins)
+        dp = [float('inf')] * (amount + 4)
+        dp[0] = 0
 
-        memo = {}
-        def check(i, curr):
-            if (i, curr) in memo:
-                return memo[(i, curr)]
-            if curr == amount:
-                return 0
-            if i == n or amount < curr:
-                return float('inf')
-            
-            pick = 1 + check(i, curr + coins[i])
-            skip = check(i + 1, curr)
-
-            memo[(i, curr)] = min(pick, skip)
-            return memo[(i, curr)]
+        for i in range(amount):
+            if dp[i] != float('inf'):
+                for c in coins:
+                    if i + c <= amount:
+                        dp[i + c] = min(dp[i + c], dp[i] + 1)
         
-        res = check(0, 0)
-        if res == float('inf'):
-            res = -1
-        
-        return res
-            
+        return dp[amount] if dp[amount] != float('inf') else -1
