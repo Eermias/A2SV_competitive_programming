cb277313361dc3ccb520e3528cadd473c4ab270f
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2025-11-24 16:56:34 +0300

    Time: 4 ms (10.27%) | Memory: 17.7 MB (95.89%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,12 +1,13 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         common = strs[0]
-        for string in strs:
-            common = common[:len(string)]
-            for i in range(len(common)):
-                if common[i] != string[i]:
-                    common = common[:i]
+        for word in strs:
+            l, r = 0, 0
+            while l < len(common) and r < len(word):
+                if common[l] != word[r]:
                     break
-            
+                l += 1
+                r += 1
+            common = common[:l]
         
         return common
